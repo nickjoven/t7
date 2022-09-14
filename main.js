4c1bb3b05f9fb73b4b1dfb0ada8e728b9161e85c
Nicholas <nicholasjoven@gmail.com>
2022-09-14 16:15:32 -0400

    added fighters grid

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-// https://tk7.tekken.com/assets/images/fighters-final/jack-7-standard/thumbnail-headshot.png
+let fighters = ["bryan", "anna"]
 
-let fighters = 
-[
-"akuma",
-"alisa",
-"anna",
-"armor-king",
-"asuka",
-"bob",
-"bryan",
-"claudio",
-"devil-jin",
-"dragunov",
-"jin",
-"josie"
-]
-
-let _fighters = fighters.map((fighter) => {
-    return fighter.toLowerCase()
-})
-
+// Recreate the Fighters grid using this function
 
 const fighterUrl = (name) => {
     return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
 }
 
-placeFighters = () => {
-    container = document.createElement('div')
-    container.classList.add('container')
-    fighters.map((name) => {
-    div = document.createElement('div')
-    div.classList.add("fighter-div")
-    img = document.createElement('img')
-    img.src = `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
-    console.log(`https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`)
-    document.body.append(container)
-    container.append(div)
-    div.append(img)
-    })
-}
 
-placeFighters()
+// https://tk7.tekken.com/assets/images/fighters-final/jack-7-standard/thumbnail-headshot.png
+
+// let fighters = 
+// [
+// "akuma",
+// "alisa",
+// "anna",
+// "armor-king",
+// "asuka",
+// "bob",
+// "bryan",
+// "claudio",
+// "devil-jin",
+// "dragunov",
+// "jin",
+// "josie"
+// ]
+
+// let _fighters = fighters.map((fighter) => {
+//     return fighter.toLowerCase()
+// })
+
+
+// const fighterUrl = (name) => {
+//     return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+// }
+
+// placeFighters = () => {
+//     container = document.createElement('div')
+//     container.classList.add('container')
+//     fighters.map((name) => {
+//     div = document.createElement('div')
+//     div.classList.add("fighter-div")
+//     img = document.createElement('img')
+//     img.src = `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+//     console.log(`https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`)
+//     document.body.append(container)
+//     container.append(div)
+//     div.append(img)
+//     })
+// }
+
+// placeFighters()
