e0da0fe812d74fffd09f6732fe1e9463df15e195
Nicholas <nicholasjoven@gmail.com>
2022-09-14 16:08:39 -0400

    initialized

---
+++ main.js
@@ -0,0 +1,43 @@
+// https://tk7.tekken.com/assets/images/fighters-final/jack-7-standard/thumbnail-headshot.png
+
+let fighters = 
+[
+"akuma",
+"alisa",
+"anna",
+"armor-king",
+"asuka",
+"bob",
+"bryan",
+"claudio",
+"devil-jin",
+"dragunov",
+"jin",
+"josie"
+]
+
+let _fighters = fighters.map((fighter) => {
+    return fighter.toLowerCase()
+})
+
+
+const fighterUrl = (name) => {
+    return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+}
+
+placeFighters = () => {
+    container = document.createElement('div')
+    container.classList.add('container')
+    fighters.map((name) => {
+    div = document.createElement('div')
+    div.classList.add("fighter-div")
+    img = document.createElement('img')
+    img.src = `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+    console.log(`https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`)
+    document.body.append(container)
+    container.append(div)
+    div.append(img)
+    })
+}
+
+placeFighters()
